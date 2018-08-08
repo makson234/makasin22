6696dac552da349b1aa7af2f81f465275e1c162c
andrew <maksim.belousov.1998@bk.ru>
2018-08-08 20:26:41 +0300

    first comment

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function setPersonage () {
     if ( document.pointCoord.length === 0 ) {
         document.demo.innerHTML = "Траектория не задана"
         return
-		}
+    }
     document.demo.innerHTML = ""
     if ( !document.personage.interval ) {
         document.personage = document.createElement ('img')
@@ -32,20 +32,20 @@ function setPersonage () {
 }
 // ---------------- Положение персонажа ----------------
 function mc_personage ( event ) {
-	 var dimX = document.pointCoord [ document.personage.nextPoint ][0] - 
-   						document.personage.coord [0]
-	 var dimY = document.pointCoord [ document.personage.nextPoint ][1] - 
-   						document.personage.coord [1]
-	 if ( Math.abs ( dimX ) < 5 && Math.abs ( dimY ) < 5 ) {
-	     if ( document.personage.nextPoint < document.pointCoord.length-1 ) {
-		     document.personage.nextPoint++
-		 }
-	     else document.personage.nextPoint = 0
-	 }
+   var dimX = document.pointCoord [ document.personage.nextPoint ][0] - 
+              document.personage.coord [0]
+   var dimY = document.pointCoord [ document.personage.nextPoint ][1] - 
+              document.personage.coord [1]
+   if ( Math.abs ( dimX ) < 5 && Math.abs ( dimY ) < 5 ) {
+       if ( document.personage.nextPoint < document.pointCoord.length-1 ) {
+         document.personage.nextPoint++
+     }
+       else document.personage.nextPoint = 0
+   }
    document.personage.style.transform = dimX < 0 ?
-						"rotateY(180deg)" : "rotateY(0deg)"
-	 document.personage.coord [0] += dimX !== 0 ? Math.sign( dimX ) * 5 : 0
-	 document.personage.coord [1] += dimY !== 0 ? Math.sign( dimY ) * 5 : 0
-	 document.personage.style.left = document.personage.coord [0] + 'px';
-	 document.personage.style.top = document.personage.coord [1] + 'px'; 
+            "rotateY(180deg)" : "rotateY(0deg)"
+   document.personage.coord [0] += dimX !== 0 ? Math.sign( dimX ) * 5 : 0
+   document.personage.coord [1] += dimY !== 0 ? Math.sign( dimY ) * 5 : 0
+   document.personage.style.left = document.personage.coord [0] + 'px';
+   document.personage.style.top = document.personage.coord [1] + 'px'; 
 }
